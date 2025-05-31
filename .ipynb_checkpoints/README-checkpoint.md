07403c09999ea1e52ae6812f53e93f29eeca8898
Samuel Tilano <stilano@uninorte.edu.co>
2025-05-31 00:50:57 -0500

    Implementacion de modularidad en el codigo, estructura inicial para empezar a desarrollar el proyecto

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,13 @@
+# ED2 PF 202510
+## To clone this repo use:
+```
+git clone https://github.com/sdariza/ED2-PF-202510.git
+```
+
+## Read MySQL Connector/Python Developer Guide
+[MySQL Connector/Python Developer Guide](https://dev.mysql.com/doc/connector-python/en/)
+
+## To use jupyter, run this command
+```
+jupyer notebook
+```
---
+++ src/database/sql_connection.py
@@ -0,0 +1,45 @@
+from mysql.connector import connect, errorcode, Error
+from os import environ
+import pandas as pd
+
+config = {
+    "user": environ['DATABASE_USERNAME'],
+    "password": environ['DATABASE_PASSWORD'],
+    "host": environ['DATABASE_HOST'],
+    "database": environ['DATABASE_NAME'],
+    "charset": 'utf8'
+}
+
+
+def get_connection():
+    try:
+        return connect(**config)
+    except Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+        return None
+
+
+def get_data(connection: connect, query: str):
+    my_cursor = connection.cursor()
+    my_cursor.execute(query)
+    data = my_cursor.fetchall()
+    my_cursor.close()
+    return data
+
+
+cnx = get_connection()
+
+print("Connection established")
+
+data = get_data(cnx, "SELECT * FROM UN.VENTAS")
+
+
+df = pd.DataFrame(data, columns=['ID_VENTA', 'FECHA_VENTA', 'ID_CLIENTE', 'ID_EMPLEADO',
+                  'ID_PRODUCTO', 'CANTIDAD', 'PRECIO_UNITARIO', 'DESCUENTO', 'FORMA_PAGO'])
+
+print(df)
